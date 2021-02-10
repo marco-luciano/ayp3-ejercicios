cb5197063730c62e42c4b001a0281456d00a3940
RamosFe <fedearamos97@gmail.com>
2021-02-10 13:24:25 -0300

    Agregado de BookPurchase y tests.

---
+++ Final - PrimerParte/book_purchase.py
@@ -0,0 +1,22 @@
+class BookPurchase:
+    def __init__(self, isbn, quantity):
+        self._validate_isbn(isbn)
+        self._validate_quantity(quantity)
+
+        self._isbn = isbn
+        self._quantity = quantity
+    
+    def _validate_isbn(self, isbn_to_validate):
+        if len(str(isbn_to_validate)) != 13:
+            raise ValueError('Ilegal ISBN')
+        
+
+    def _validate_quantity(self, quantity_to_validate):
+        if quantity_to_validate < 1:
+            raise ValueError('Quantity must be greater or equal to 1')
+
+    def get_isbn(self):
+        return self._isbn
+    
+    def get_quantity(self):
+        return self._quantity
---
+++ Final - PrimerParte/test_book_purchase.py
@@ -0,0 +1,33 @@
+import unittest
+from book_purchase import BookPurchase
+
+class TestBookPurchase(unittest.TestCase):
+    def test00_book_saves_correctly_the_isbn_and_quantity(self):
+        testing_isbn = '1234567891234'
+        testing_quantity = 1
+        testing_book_purchase = BookPurchase(testing_isbn, testing_quantity)
+
+        self.assertEqual(testing_isbn, testing_book_purchase.get_isbn())
+        self.assertEqual(testing_quantity, testing_book_purchase.get_quantity())
+    
+    def test01_invalid_isbn_should_raise_error(self):
+        ilegal_isbn = '1'
+        testing_quantity = 1
+
+        with self.assertRaises(ValueError) as error:
+            testing_book_purchase = BookPurchase(ilegal_isbn, testing_quantity)
+        
+        self.assertEqual('Ilegal ISBN', str(error.exception))
+    
+    def test02_invalid_quantity_should_raise_error(self):
+        testing_isbn = '1234567891234'
+        ilegal_quantity = 0
+
+        with self.assertRaises(ValueError) as error:
+            testing_book_purchase = BookPurchase(testing_isbn, ilegal_quantity)
+        
+        self.assertEqual('Quantity must be greater or equal to 1', str(error.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
