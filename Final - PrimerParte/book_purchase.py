ba5783d9a1cbb4274ab7dcb7d8e258462e03357a
RamosFe <fedearamos97@gmail.com>
2021-02-18 12:10:47 -0300

    Agregado de precio al BookPurchase y agregado de 2 pruebas al mismo.

--- Final - PrimerParte/book_purchase.py
+++ Final - PrimerParte/book_purchase.py
@@ -1,10 +1,12 @@
 class BookPurchase:
-    def __init__(self, isbn, quantity):
+    def __init__(self, isbn, quantity, price_per_unit):
         self._validate_isbn(isbn)
         self._validate_quantity(quantity)
+        self._validate_price(price_per_unit)
 
         self._isbn = isbn
         self._quantity = quantity
+        self._price_per_unit = price_per_unit
     
     def _validate_isbn(self, isbn_to_validate):
         if len(str(isbn_to_validate)) != 13:
@@ -14,8 +16,15 @@ class BookPurchase:
         if quantity_to_validate < 1:
             raise ValueError('Quantity must be greater or equal to 1')
 
+    def _validate_price(self, price_per_unit):
+        if price_per_unit < 0:
+            raise ValueError('Price must be equal or greater to 0')
+    
     def get_isbn(self):
         return self._isbn
     
     def get_quantity(self):
         return self._quantity
+    
+    def get_total_price(self):
+        return self._price_per_unit * self._quantity
--- Final - PrimerParte/test_book_purchase.py
+++ Final - PrimerParte/test_book_purchase.py
@@ -5,28 +5,51 @@ class TestBookPurchase(unittest.TestCase):
     def test00_book_saves_correctly_the_isbn_and_quantity(self):
         testing_isbn = '1234567891234'
         testing_quantity = 1
-        testing_book_purchase = BookPurchase(testing_isbn, testing_quantity)
+        testing_price = 25
+
+        testing_book_purchase = BookPurchase(testing_isbn, testing_quantity, testing_price)
 
         self.assertEqual(testing_isbn, testing_book_purchase.get_isbn())
         self.assertEqual(testing_quantity, testing_book_purchase.get_quantity())
     
-    def test01_invalid_isbn_should_raise_error(self):
+    def test01_get_total_price_of_the_book_purchase(self):
+        testing_isbn = '1234567891234'
+        testing_quantity = 3
+        testing_price = 25
+
+        testing_book_purchase = BookPurchase(testing_isbn, testing_quantity, testing_price)
+
+        self.assertEqual(75, testing_book_purchase.get_total_price())
+    
+    def test02_invalid_isbn_should_raise_error(self):
         ilegal_isbn = '1'
         testing_quantity = 1
+        testing_price = 25
 
         with self.assertRaises(ValueError) as error:
-            testing_book_purchase = BookPurchase(ilegal_isbn, testing_quantity)
+            testing_book_purchase = BookPurchase(ilegal_isbn, testing_quantity, testing_price)
         
         self.assertEqual('Illegal ISBN', str(error.exception))
     
-    def test02_invalid_quantity_should_raise_error(self):
+    def test03_invalid_quantity_should_raise_error(self):
         testing_isbn = '1234567891234'
         ilegal_quantity = 0
+        testing_price = 25
 
         with self.assertRaises(ValueError) as error:
-            testing_book_purchase = BookPurchase(testing_isbn, ilegal_quantity)
+            testing_book_purchase = BookPurchase(testing_isbn, ilegal_quantity, testing_price)
         
         self.assertEqual('Quantity must be greater or equal to 1', str(error.exception))
+    
+    def test04_invalid_price_per_unit_should_raise_error(self):
+        testing_isbn = '1234567891234'
+        testing_quantity = 3
+        invalid_price = -4
+
+        with self.assertRaises(ValueError) as error:
+            testing_book_purchase = BookPurchase(testing_isbn, testing_quantity, invalid_price)
+        
+        self.assertEqual('Price must be equal or greater to 0', str(error.exception))
 
 
 if __name__ == '__main__':
